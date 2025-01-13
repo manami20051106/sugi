18ab9da0b27517438a94c1b0e641be671cfc270a
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2025-01-13 21:04:20 +0900

    Update ex1-4.js

--- ex1-4.js
+++ ex1-4.js
@@ -1,3 +1,3 @@
-function nazo79(){if(document.getElementById('answer79').value=='×'){alert('あたり！');}
-                  else if(document.getElementById('answer79').value=='ばつ'){alert('あたり！');}
+function nazo80(){if(document.getElementById('answer80').value=='×'){alert('あたり！');}
+                  else if(document.getElementById('answer80').value=='ばつ'){alert('あたり！');}
                   else{alert('はずれ！');}}
