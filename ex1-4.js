b62d8ca40bdfbce42aa4751eef772d8063bf9e58
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2025-01-13 21:04:00 +0900

    Update and rename ex1-4,js to ex1-4.js

---
+++ ex1-4.js
@@ -0,0 +1,3 @@
+function nazo79(){if(document.getElementById('answer79').value=='×'){alert('あたり！');}
+                  else if(document.getElementById('answer79').value=='ばつ'){alert('あたり！');}
+                  else{alert('はずれ！');}}
