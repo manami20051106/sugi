1857df92cbff92c2aa15d6f4baea815f2e801e43
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2025-01-13 20:01:26 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,1 +1,3 @@
-
+function nazo80(){if(document.getElementById('answer80').value=='〇'){alert('あたり！');}
+                  else if(document.getElementById('answer80').value=='まる'){alert('あたり！');}
+                  else{alert('はずれ！');}}
